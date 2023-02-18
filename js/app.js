e9d798387295e2a1aaa77cc67f87facb649424d5
FowziaSultana <fowziasultana168@gmail.com>
2023-02-18 15:41:24 +0600

    parallelogram btn js added

--- js/app.js
+++ js/app.js
@@ -15,6 +15,9 @@ function addList(name, result) {
     `;
   parent.appendChild(liElement);
 }
+
+//-------------------------------------------------- triangle btn js---------------------------------------
+
 document
   .getElementById("calculate-triangle-btn")
   .addEventListener("click", function () {
@@ -29,6 +32,9 @@ document
       addList("Triangle", area);
     }
   });
+
+//-------------------------------------------------- rectangle btn js---------------------------------------
+
 document
   .getElementById("calculate-rectangle-btn")
   .addEventListener("click", function () {
@@ -41,5 +47,22 @@ document
     } else {
       const area = (parseFloat(width) * parseFloat(length)).toFixed(2);
       addList("Rectangle", area);
+    }
+  });
+
+//-------------------------------------------------- parallelogram btn js---------------------------------------
+
+document
+  .getElementById("calculate-parallelogram-btn")
+  .addEventListener("click", function () {
+    const base = getValueFromInput("parallelogram-b");
+    const height = getValueFromInput("parallelogram-h");
+    if (isNaN(base) || isNaN(height)) {
+      alert("please enter a valid number");
+    } else if (base <= 0 || height <= 0) {
+      alert("please give a positive number");
+    } else {
+      const area = (parseFloat(base) * parseFloat(height)).toFixed(2);
+      addList("Parallelogram", area);
     }
   });
